3ef2079c2264cb7d8893f58afe7e75c308a5126e
Luciano Pellacani Franca <t203666@tce.es.gov.br>
2016-11-21 18:50:47 -0200

    fixing a few stuff and making it safe

--- src/images/utils.js
+++ src/images/utils.js
@@ -1,14 +1,25 @@
+const supportedFormats = ['image/png', 'image/jpeg', 'image/bpm']
+
 function setupImages () {
-  let spanText = document.querySelectorAll('span')[0]
+  let contentBox = document.getElementsByClassName('content')[0]
   document.ondragover = () => false
   document.ondragend = () => false
   document.ondrop = (e) => {
     e.preventDefault()
-    const file = e.dataTransfer.files[0]
-    const reader = new FileReader()
-    reader.onload = (event) => spanText.innerHTML += `<img src="${event.target.result}"></img>`
-    reader.readAsDataURL(file)
-    return false
+    console.log(e.dataTransfer.files)
+    if (e.dataTransfer.files.length) {
+      const file = e.dataTransfer.files[0]
+      if (supportedFormats.indexOf(file.type) !== -1) {
+        const reader = new FileReader()
+        reader.onload = (event) => {
+          let newImage = document.createElement('img');
+          newImage.src = event.target.result;
+          contentBox.appendChild(newImage);
+        }
+        reader.readAsDataURL(file)
+        return false
+      }
+    }
   }
 }
 
