21e2e61b5f938f0d4f911d8834122f2a6e380bd1
Luciano Pellacani Franca <t203666@tce.es.gov.br>
2016-11-21 16:07:18 -0200

    default text transformations working

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const shortcutKeycodes = {
+  B: { code: 66, action: 'bold' },
+  I: { code: 73, action: 'italic' },
+  U: { code: 85, action: 'underline' }
+}
+
+function dispatchTransform (transformName) {
+  document.execCommand(transformName, false, null);
+}
+
+function preventAndDispatch (transformName, e) {
+  e.preventDefault();
+  e.stopPropagation();
+  dispatchTransform(transformName)
+}
+
+function setupKeys () {
+  document.onkeydown = (e) => {
+    e = e || window.event
+    if (e.ctrlKey) {
+      let c = e.which || e.keyCode
+      Object.keys(shortcutKeycodes).forEach((key) => {
+        if (c === shortcutKeycodes[key].code) {
+          preventAndDispatch(shortcutKeycodes[key].action, e)
+        }
+      })
+    }
+  }
+}
+
+window.onload = () => {
+  console.log('Setting things up')
+	document.designMode = 'On';
+  document.execCommand("styleWithCSS",false,false)
+  if (navigator.vendor !== 'Google Inc.') {
+    setupKeys()
+  }
+}
